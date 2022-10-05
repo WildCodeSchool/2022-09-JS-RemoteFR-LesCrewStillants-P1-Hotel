2392ebe439fa8e08c75f652b6d2c1a988525f600
NicolasBANIC <113355022+NicolasBANIC@users.noreply.github.com>
2022-10-05 15:35:08 +0200

    fixxxx

---
+++ assets/js/contact.js
@@ -0,0 +1,4 @@
+const send = document.querySelector(".formulaire-btn").addEventListener("click", function(){ alert("Thank you for your message !"); });
+
+//if(alert("Thank you for your message !")){}
+//else    window.location.reload(); 
